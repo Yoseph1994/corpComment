9c253523feb464d620a27963146a1e0b8cebde50
Yoseph1994 <joshrde2002@example.com>
2024-07-27 09:02:04 +0300

    basic feedback list layout

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,5 +1,28 @@
+import { TriangleUpIcon } from "@radix-ui/react-icons";
 function FeedbackList() {
-  return <ol className="feedback-list">FeedbackList</ol>;
+  return (
+    <ol className="feedback-list">
+      <li className="feedback">
+        <button>
+          <TriangleUpIcon />
+          <span>670</span>
+        </button>
+
+        <div className="">
+          <p>B</p>
+        </div>
+        <div className="">
+          <p>ByteGrad</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quo
+            inventore obcaecati, iure eaque alias eligendi tempora blanditiis
+            sunt aperiam atque, itaque architecto aspernatur reiciend
+          </p>
+        </div>
+        <p>4 days ago</p>
+      </li>
+    </ol>
+  );
 }
 
 export default FeedbackList;
